fb021d7eb11d34851c5047b8c8b828ca347cdc79
Alchemist723 <60349199+Alchemist723@users.noreply.github.com>
2022-06-25 12:11:40 +0800

    这是Nchu180962班蹭饭地图

---
+++ js/data.js
@@ -0,0 +1,213 @@
+let students=
+[
+    {
+        "name": "李文龙",
+        "school": " ",
+        "city": "武汉",
+        "province": "湖北"
+    },
+    {
+        "name": "陈伊凡",
+        "school": " ",
+        "city": "唐山",
+        "province": "河北"
+    },
+    {
+        "name": "何珊",
+        "school": " ",
+        "city": "南昌",
+        "province": "江西"
+    },
+    {
+        "name": "童潇敏",
+        "school": " ",
+        "city": "南昌",
+        "province": "江西"
+    },
+    {
+        "name": "刘文静",
+        "school": " ",
+        "city": "临汾",
+        "province": "山西"
+    },
+    {
+        "name": "卞晨妤",
+        "school": " ",
+        "city": "杭州",
+        "province": "浙江"
+    },
+    {
+        "name": "冯乐强",
+        "school": " ",
+        "city": "广州",
+        "province": "广东"
+    },
+    {
+        "name": "张嘉枥",
+        "school": " ",
+        "city": "深圳",
+        "province": "广东"
+    },
+    {
+        "name": "凌翔",
+        "school": " ",
+        "city": "九江",
+        "province": "江西"
+    },
+    {
+        "name": "吴梓燃",
+        "school": " ",
+        "city": "九江",
+        "province": "江西"
+    },
+    {
+        "name": "吕洋",
+        "school": "燕山大学",
+        "city": "秦皇岛",
+        "province": "河北"
+    },
+    {
+        "name": "管明辉",
+        "school": " ",
+        "city": "景德镇",
+        "province": "江西"
+    },
+    {
+        "name": "张文",
+        "school": "东北师范大学",
+        "city": "长春",
+        "province": "吉林"
+    },
+    {
+        "name": "张沛嵘",
+        "school": " ",
+        "city": "深圳",
+        "province": "广东"
+    },
+    {
+        "name": "陈利",
+        "school": " ",
+        "city": "深圳",
+        "province": "广东"
+    },
+    {
+        "name": "黄宁",
+        "school": " ",
+        "city": "南昌",
+        "province": "江西"
+    },
+    {
+        "name": "李旭翔",
+        "school": " ",
+        "city": "南昌",
+        "province": "江西"
+    },
+    {
+        "name": "刘永川",
+        "school": " ",
+        "city": "深圳",
+        "province": "广东"
+    },
+    {
+        "name": "阙圣武",
+        "school": " ",
+        "city": "东莞",
+        "province": "广东"
+    },
+    {
+        "name": "沈齐峰",
+        "school": " ",
+        "city": "深圳",
+        "province": "广东"
+    },
+    {
+        "name": "谭天浩",
+        "school": "北京化工大学",
+        "city": "朝阳区",
+        "province": "北京"
+    },
+    {
+        "name": "王焱",
+        "school": "杭州电子科技大学",
+        "city": "杭州",
+        "province": "浙江"
+    },
+    {
+        "name": "吴煜杭",
+        "school": "南昌航空大学",
+        "city": "南昌",
+        "province": "江西"
+    },
+    {
+        "name": "项允",
+        "school": " ",
+        "city": "深圳",
+        "province": "广东"
+    },
+    {
+        "name": "谢东东",
+        "school": " ",
+        "city": " ",
+        "province": "上海"
+    },
+    {
+        "name": "徐俊",
+        "school": " ",
+        "city": "南昌",
+        "province": "江西"
+    },
+    {
+        "name": "徐汪洋",
+        "school": " ",
+        "city": "南昌",
+        "province": "江西"
+    },
+    {
+        "name": "杨成凯",
+        "school": "南昌大学",
+        "city": "南昌",
+        "province": "江西"
+    },
+    {
+        "name": "姚艳军",
+        "school": "南昌航空大学",
+        "city": "南昌",
+        "province": "江西"
+    },
+    {
+        "name": "曾鑫",
+        "school": " ",
+        "city": "南昌",
+        "province": "江西"
+    },
+    {
+        "name": "张春求",
+        "school": " ",
+        "city": " ",
+        "province": "上海"
+    },
+    {
+        "name": "宗正昌",
+        "school": "南昌航空大学",
+        "city": "南昌",
+        "province": "江西"
+    },
+    {
+        "name": "邹裕伟",
+        "school": "重庆邮电大学",
+        "city": "南岸区",
+        "province": "重庆"
+    },
+    {
+        "name": "和海洋",
+        "school": " ",
+        "city": "南昌",
+        "province": "江西"
+    },
+    {
+        "name": "李国力",
+        "school": " ",
+        "city": "宁德",
+        "province": "福建"
+    }
+]
